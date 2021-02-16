aaf1b37fd6967bed8f1761c8c8b5822482b28475
@diaz.nahuel <nahuel.diazz.nd@gmail.com>
2021-02-16 03:15:26 -0300

    algo

---
+++ Proyect/ecommerce/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1acd13cd5eb35eba14f879bee1e4db49'),
+    },
+  },
+});
---
+++ Proyect/ecommerce/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e2a09f48-fc2f-4624-89f6-38eba63c72dc'
+};
